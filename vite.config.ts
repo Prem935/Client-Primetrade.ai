ea3e607e946db6129d1c166c0eb5a310997193f4
Prem935 <prem886044@gmail.com>
2025-10-02 19:56:09 +0530

    fix backend address

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   const target = "https://primetrade-backend-5lak.onrender.com";
   return {
     plugins: [react()],
